e34058b2a747c9a94e3fe7f278c0b65c7e230573
Tomas Johanovsky <tjohanovsky@pm.me>
2026-01-03 13:01:49 +0100

    feat: add CLI support with urfave/cli v3.6.1

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/johanovskyt/kata-to-go
 
 go 1.25.5
+
+require github.com/urfave/cli/v3 v3.6.1 // indirect
--- main.go
+++ main.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
 
+	"github.com/urfave/cli/v3"
+)
+
+func main() {
+	cmd := &cli.Command{
+		Name:   "new",
+		Usage:  "Create a new project",
+		Action: newProject,
+	}
+
+	if err := cmd.Run(context.Background(), os.Args); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func newProject(ctx context.Context, command *cli.Command) error {
+	fmt.Println("TODO")
+
+	return nil
 }
